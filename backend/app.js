6f9136c88baa079143935c38cf6c43b57d3af31e
Rekun Ivan <vana.rekun@mail.ru>
2023-05-27 18:25:49 +0400

    feat: added logs and .env

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
-app.use(requestLogger());
+app.use(requestLogger);
 
 app.get('/crash-test', () => {
   setTimeout(() => {
@@ -40,7 +40,7 @@ app.use('*', ((req, res, next) => {
   return next(err);
 }));
 
-app.use(errorLogger());
+app.use(errorLogger);
 app.use(errors());
 app.use(require('./middlewares/error'));
 
